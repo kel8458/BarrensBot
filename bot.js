9a2af4e6a2b40e58e393d40e1ddf5f4386e32366
pjfry651 <pjfry651@gmail.com>
2018-06-12 11:03:20 +1000

    Item lookups

--- bot.js
+++ bot.js
@@ -18,6 +18,7 @@ var discordServer = undefined;
 var isWorldServerOnline = true;
 var worldServerNotificationChannel = undefined;
 var itemNames = [];
+var itemLinkCooldowns = {};
 
 // Database
 var worldDB = mysql.createConnection(CONFIG.worldDB);
@@ -85,7 +86,142 @@ client.on('message', message => {
     message.author.send(CONFIG.welcomeMessage);
     return;
   }
+
+  // Item lookup by ID
+	const itemLookupIdRegex = /\[(\d*?)\]/;
+	var itemLookupIdMatches = message.content.match(itemLookupIdRegex);
+	if (itemLookupIdMatches) {
+		if (IsItemLookupOnCooldown(message, message.author.id))
+			return;
+
+		var itemId = itemLookupIdMatches[1];
+
+		worldDB.query("SELECT IT.entry, IDI.field5 FROM item_template IT INNER JOIN item_display_info IDI ON IT.displayid = IDI.field0 WHERE IT.entry = ?", [itemId], function (error, results, fields) {
+			if (error) throw error;
+
+			if (!!results && results.length > 0)
+			{
+				var id = results[0].entry;
+				var image = results[0].field5;
+				sendItemScreenshot(id, image, message);
+			}
+		});
+
+		return;
+	}
+
+	// Item lookup by name
+	const itemLookupRegex = /\[(.*?)\]/;
+	var itemLookupMatches = message.content.match(itemLookupRegex);
+	if (itemLookupMatches) {
+		var didYouMeanActive = false;
+		var suggestedItem = '';
+
+		if (IsItemLookupOnCooldown(message, message.author.id))
+			return;
+
+		var itemName = itemLookupMatches[1];
+		if (!itemNames.includes(itemName.toLowerCase()))
+		{
+			suggestedItem = didYouMean(itemName, itemNames);
+			if (!!suggestedItem)
+				didYouMeanActive = true;
+		}
+
+		worldDB.query("SELECT IT.entry, IT.name, IDI.field5 FROM item_template IT INNER JOIN item_display_info IDI ON IT.displayid = IDI.field0 WHERE IT.name = ?", [didYouMeanActive ? suggestedItem : itemName], function (error, results, fields) {
+			if (error) throw error;
+
+			if (!!results && results.length > 0)
+			{
+				var id = results[0].entry;
+				var image = results[0].field5;
+				var name = results[0].name;
+				sendItemScreenshot(id, image, message, itemName, didYouMeanActive ? name : '');
+			}
+		});
+
+		return;
+	}
+
+	// Item lookup by URL
+	const itemLookupUrlRegex = /db\.classicdb\.ch\/\?item=(.\d*)/;
+	var itemLookupUrlMatches = message.content.match(itemLookupUrlRegex);
+	if (itemLookupUrlMatches) {
+		if (IsItemLookupOnCooldown(message, message.author.id))
+			return;
+
+		var itemId = itemLookupUrlMatches[1];
+
+		var itemId = itemLookupUrlMatches[1];
+		worldDB.query("SELECT IT.entry, IDI.field5 FROM item_template IT INNER JOIN item_display_info IDI ON IT.displayid = IDI.field0 WHERE IT.entry = ?", [itemId], function (error, results, fields) {
+			if (error) throw error;
+
+			if (!!results && results.length > 0)
+			{
+				var id = results[0].entry;
+				var image = results[0].field5;
+				sendItemScreenshot(id, image, message);
+			}
+		});
+
+		return;
+	}
 });
+
+var sendItemScreenshot = function (id, image, message, itemName, suggestedItem) {
+	var filenamePng = "./items/png/" + id + ".png";
+	var filenameHtml = "./items/html/" + id + ".html";
+	var url = CONFIG.siteUrl + "tooltip/1/" + id + '/html';
+	var itemTooltipUrl = CONFIG.siteUrl + "tooltip/1/" + id;
+	var itemTemplateUrl = "./item-tooltip.html";
+	var itemUrl = CONFIG.siteUrl + "item/1/" + id;
+	var vdbUrl = "http://classicdb.ch/?item=" + id;
+
+	if (fs.existsSync(filenamePng)) {
+	    message.channel.send(
+			!!suggestedItem ? "I couldn't find [" + itemName + "], did you mean [" + suggestedItem + "]?" : "here's your item!\n" +
+			"**" + CONFIG.siteName + " DB:** " + itemUrl + "\n" +
+			"**Classic DB:** " + vdbUrl, { reply: message.author, files: [filenamePng]});
+		return;
+	}
+
+	webshot(url, filenamePng, { captureSelector: '.item_space', renderDelay: 1000 }, function (err) {
+		if (!!err) console.log(err);
+
+	    message.channel.send(
+			!!suggestedItem ? "I couldn't find [" + itemName + "], did you mean [" + suggestedItem + "]?" : "here's your item!\n" +
+			"**" + CONFIG.siteName + " DB:** " + itemUrl + "\n" +
+			"**Classic DB:** " + vdbUrl, { reply: message.author, files: [filenamePng]});
+	});
+}
+
+var IsItemLookupOnCooldown = function (message, userId) {
+	var now = new Date();
+	var nowMS = now.getTime();
+
+	if (!!itemLinkCooldowns[userId] && (nowMS - itemLinkCooldowns[userId] < CONFIG.itemLinkCooldownMS))
+	{
+		var randomMessage = config.cooldownWarnings[Math.floor(Math.random() * config.cooldownWarnings.length)];
+		message.channel.send("**" + randomMessage + "** Item lookups have a 5 second cooldown. Please wait a few seconds before looking up another item.", { reply: message.author });
+		return true;
+	}
+
+	itemLinkCooldowns[userId] = nowMS;
+	return false;
+}
+
+var download = function(url, dest, cb) {
+	var file = fs.createWriteStream(dest);
+	var request = https.get(url, function(response) {
+		response.pipe(file);
+		file.on('finish', function() {
+			file.close(cb);  // close() is async, call cb after close completes.
+		});
+	}).on('error', function(err) { // Handle errors
+		fs.unlink(dest); // Delete the file async. (But we don't check the result)
+		if (cb) cb(err.message);
+	});
+};
 
 var downtimeNotifierUpdate = function () {
   var processExists = false;
